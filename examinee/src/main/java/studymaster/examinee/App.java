4bbfd2607cb72fb66a98556f737b789973b0d110
cxxkxxm <xc540863058@gmail.com>
2014-02-25 21:33:01 +0800

    reewqr

--- examinee/src/main/java/studymaster/examinee/App.java
+++ examinee/src/main/java/studymaster/examinee/App.java
@@ -1,15 +1,3 @@
-package studymaster.examinee;
-
-import studymaster.socket.Connector;
-import studymaster.all.Model.Client;
-import studymaster.all.ViewController.Director;
-
-public class App extends Client {
-
-	public static void main(String[] args) throws Exception {
-		//Configure
-		Connector.setServer(Configure.SERVER);
-		Connector.setEndpoint(Configure.ENDPOINT);
 
 		//launchvk;ldskffds;lf
 	}
